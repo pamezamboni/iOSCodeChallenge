f63799e7953cef0c159718787e4f2a6ae105b9bd
Pamela Zamboni <pamela.zamboni@mawape.com>
2019-04-11 14:26:33 -0300

    split layout initial setup

---
+++ iOSCodeChallenge/Scenes/Posts/PostsViewController.swift
@@ -0,0 +1,30 @@
+//
+//  PostsViewController.swift
+//  iOSCodeChallenge
+//
+//  Created by Pamela Zamboni on 11/04/2019.
+//  Copyright © 2019 Pamela Zamboni. All rights reserved.
+//
+
+import UIKit
+
+class PostsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
